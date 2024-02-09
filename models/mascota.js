12711c19dcf834783fe4dcec18649bd57815058f
Informatica <informatica@alumno.local>
2024-02-09 12:03:15 -0600

    Agregando sistema de adopcion de mascotas

---
+++ models/mascota.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require('mongoose');
+
+const MascotaSchema = new Schema({
+    nombre: {
+        type: String,
+        required: true
+    },
+    edad: {
+        type: Number,
+        required: true
+    },
+    especie: {
+        type: String,
+        required: true
+    },
+    descripcion: String,
+    adoptada: {
+        type: Boolean,
+        default: false
+    }
+});
+
+module.exports = model('Mascota', MascotaSchema);
